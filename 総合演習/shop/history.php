ce704b9b2a9bc6e2b2f2d35d8c8598f6627a31d9
odd-star <75000097+odd-star@users.noreply.github.com>
2021-02-12 09:58:25 +0900

    Add files via upload

---
+++ 総合演習/shop/history.php
@@ -0,0 +1,26 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>購入履歴画面</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	<?php require 'menu.php'; ?>
+	<?php
+	    //ログインの確認
+		if (isset($_SESSION['customer'])){
+			//MYSQLデータベースに接続する
+			require 'db_connect.php';
+		
+		}else{
+	?>
+	購入履歴を表示するには、ログインしてください。
+	<?php
+		}
+	?>
+</body>
+</html>
